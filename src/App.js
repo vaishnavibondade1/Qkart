3faf791890df78fc6585642fe575f9ea0b93e50b
Nico Robin <luffy@saitama>
2024-07-01 16:25:22 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import Register from "./components/Register";
+import ipConfig from "./ipConfig.json";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Login from "./components/Login";
+import Products from "./components/Products";
+import Checkout from "./components/Checkout";
+import Thanks from "./components/Thanks"
+
+export const config = {
+  endpoint: `http://${ipConfig.workspaceIp}:8082/api/v1`,
+};
+
+ 
+function App() {
+  return (
+    <div className="App">
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/">
+                    <Products />
+                </Route>
+                <Route path="/Register">
+                    <Register />
+                </Route>
+                <Route path="/Login">
+                    <Login />
+                </Route>
+
+                <Route path="/checkout">
+                        <Checkout />
+                    </Route>
+                    <Route path="/thanks">
+                        <Thanks />
+                    </Route>
+
+
+            </Switch>
+        </BrowserRouter>
+    </div>
+);
+}
+
+export default App;
